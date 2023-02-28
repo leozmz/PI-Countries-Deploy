2ef712ee1cde75618187c4f8e0c3f1e68c5f50c2
zm0x7b9 <zm0x7b9@gmail.com>
2023-02-28 13:34:02 -0300

    modificaciones varias

--- api/index.js
+++ api/index.js
@@ -19,7 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001;
+
+require('dotenv').config();
+const { PORT } = process.env;
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
